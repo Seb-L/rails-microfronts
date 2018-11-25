3cbb42871f9a32e542833a1afa88be6e026fc193
Seb.L <sebastien.lombard75@gmail.com>
2018-11-25 22:48:00 +0100

    track front builds for demo purposes

---
+++ micro-fronts/react-app/build/precache-manifest.69737ff76d2dfa672e1c70f4fd1cff09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12ea0ee2551613e7e6c1",
+    "url": "/static/js/main.12ea0ee2.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "12ea0ee2551613e7e6c1",
+    "url": "/static/css/main.885cfa86.chunk.css"
+  },
+  {
+    "revision": "01b7c4a3933c51bcb829287e0b0b7aac",
+    "url": "/index.html"
+  }
+];
